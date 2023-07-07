ae4fbdb17957a3806b2b668242533d9066e0e061
Aezikel <donzikky1@gmail.com>
2023-07-07 17:10:10 +0100

    Initial commit calculator xml

---
+++ app/src/main/java/com/example/calculator/CalculatorActivity.java
@@ -0,0 +1,17 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+//import androidx.viewbinding.ViewBinding;
+
+import android.os.Bundle;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_calculator);
+    }
+}
