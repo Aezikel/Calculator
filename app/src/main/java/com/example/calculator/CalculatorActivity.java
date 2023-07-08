4b5d0b8f61b48fe9b79753a31a885335c7ac4f88
Aezikel <donzikky1@gmail.com>
2023-07-08 21:43:12 +0100

    fixed zero first input bug

--- app/src/main/java/com/example/calculator/CalculatorActivity.java
+++ app/src/main/java/com/example/calculator/CalculatorActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.text.InputType;
+import android.text.TextUtils;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -12,8 +13,8 @@ import com.example.calculator.databinding.ActivityCalculatorBinding;
 
 
 public class CalculatorActivity extends AppCompatActivity {
-    private ActivityCalculatorBinding binding;
-
+     ActivityCalculatorBinding binding;
+     StringBuilder builder;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -22,6 +23,21 @@ public class CalculatorActivity extends AppCompatActivity {
         binding = ActivityCalculatorBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        builder = new StringBuilder();
+        final int ZERO = 0;
+        final int ONE = 1;
+        final int TWO = 2;
+        final int THREE = 3;
+        final int FOUR = 4;
+        final int FIVE = 5;
+        final int SIX = 6;
+        final int SEVEN = 7;
+        final int EIGHT = 8;
+        final int NINE = 9;
+        final String decimal_point = ".";
+
+
 
         binding.userInputEditTextCalculatorActivity.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -32,7 +48,131 @@ public class CalculatorActivity extends AppCompatActivity {
                 binding.userInputEditTextCalculatorActivity.setInputType(inType); // restore input type
                 return true;            }
         });
+
+
+        binding.zeroBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (builder.length() != 0) {
+                    if (builder.length() == 1 && TextUtils.equals(String.valueOf(builder.charAt(0)), String.valueOf(ZERO))) {
+                        return;
+                    }
+                }
+                builder.append(ZERO);
+                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
+            }
+        });
+
+        binding.doubleZeroBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                builder.append(ZERO);
+                builder.append(ZERO);
+                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
+            }
+        });
+
+        binding.oneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    initZero(ONE);
+                    builder.append(ONE);
+                    binding.userInputEditTextCalculatorActivity.setText(builder.toString());
+            }
+        });
+
+
+        binding.twoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initZero(TWO);
+                builder.append(TWO);
+                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
+            }
+        });
+
+        binding.threeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initZero(THREE);
+                builder.append(THREE);
+                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
+            }
+        });
+        binding.fourBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initZero(FOUR);
+                builder.append(FOUR);
+                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
+            }
+        });
+        binding.fiveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initZero(FIVE);
+                builder.append(FIVE);
+                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
+            }
+        });
+        binding.sixBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initZero(SIX);
+                builder.append(SIX);
+                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
+            }
+        });
+        binding.sevenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initZero(SEVEN);
+                builder.append(SEVEN);
+                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
+            }
+        });
+        binding.eightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initZero(EIGHT);
+                builder.append(EIGHT);
+                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
+            }
+        });
+
+        binding.nineBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initZero(NINE);
+                builder.append(NINE);
+                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
+            }
+        });
+
+
+
+
+
+
+        binding.cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                builder.setLength(0);
+                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
+            }
+        });
+
     }
+
+    protected void initZero(int number){
+        if (builder.length() == 1 && TextUtils.equals(String.valueOf(builder.charAt(0)), "0")){
+            builder.setCharAt(0, (char) number);
+        }
+    }
+
+
 
 
 }
