1c7ad4d2fa06f5c8bac0abb523a0372c1ef45faf
Aezikel <donzikky1@gmail.com>
2023-07-08 17:32:25 +0100

    xml: activity_calculator.xml light and dark mode implemented

--- app/src/main/java/com/example/calculator/CalculatorActivity.java
+++ app/src/main/java/com/example/calculator/CalculatorActivity.java
@@ -1,17 +1,38 @@
 package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-//import androidx.viewbinding.ViewBinding;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.example.calculator.databinding.ActivityCalculatorBinding;
+
 
 public class CalculatorActivity extends AppCompatActivity {
+    private ActivityCalculatorBinding binding;
 
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityCalculatorBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        setContentView(R.layout.activity_calculator);
+        binding.userInputEditTextCalculatorActivity.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int inType = binding.userInputEditTextCalculatorActivity.getInputType();
+                binding.userInputEditTextCalculatorActivity.setInputType(InputType.TYPE_NULL); // disable soft input
+                binding.userInputEditTextCalculatorActivity.onTouchEvent(motionEvent); // call native handler
+                binding.userInputEditTextCalculatorActivity.setInputType(inType); // restore input type
+                return true;            }
+        });
     }
+
+
 }
