bedd0d2a6465c05fcf377e6b943aa9e97e8a3a36
Aezikel <donzikky1@gmail.com>
2023-07-14 23:18:30 +0100

    refactor initZero onClick zerobtn, onebtn

--- app/src/main/java/com/example/calculator/CalculatorActivity.java
+++ app/src/main/java/com/example/calculator/CalculatorActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.InputType;
 import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.calculator.databinding.ActivityCalculatorBinding;
+
+import java.util.Objects;
 
 
 public class CalculatorActivity extends AppCompatActivity {
@@ -17,6 +23,7 @@ public class CalculatorActivity extends AppCompatActivity {
      StringBuilder builder;
      boolean canDecimal;
      boolean canAppendZeroZero;
+     int cursorPosition;
 
 
     @SuppressLint("ClickableViewAccessibility")
@@ -49,28 +56,43 @@ public class CalculatorActivity extends AppCompatActivity {
         canDecimal = true;
         canAppendZeroZero = true;
 
-        binding.userInputEditTextCalculatorActivity.setOnTouchListener(new View.OnTouchListener() {
+        binding.userInputEditText.setShowSoftInputOnFocus(false);
+
+        binding.userInputEditText.addTextChangedListener(new TextWatcher() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                int inType = binding.userInputEditTextCalculatorActivity.getInputType();
-                binding.userInputEditTextCalculatorActivity.setInputType(InputType.TYPE_NULL); // disable soft input
-                binding.userInputEditTextCalculatorActivity.onTouchEvent(motionEvent); // call native handler
-                binding.userInputEditTextCalculatorActivity.setInputType(inType); // restore input type
-                return true;            }
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!TextUtils.isEmpty(binding.userInputEditText.getText())
+                        && binding.userInputEditText.length() == 1){
+                            binding.userInputEditText.requestFocus();
+                }
+            }
         });
+
+
 
 
         binding.zeroBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!builder.toString().isEmpty()) {
-                    if (builder.length() == 1 && TextUtils.equals(String.valueOf(builder.charAt(0)), "0")) {
-                        return;
+                if (!TextUtils.isEmpty(binding.userInputEditText.getText())) {
+                    if (binding.userInputEditText.length() == 1
+                            && binding.userInputEditText.getText().toString().charAt(0) == '0') {
+                                return;
                     }
                     if (checkOperator()){
                         builder.append(ZERO);
+                        binding.userInputEditText.append(String.valueOf(ZERO));
                         canAppendZeroZero = false;
-                        binding.userInputEditTextCalculatorActivity.setText(builder.toString());
 
                         return;
 
@@ -81,211 +103,232 @@ public class CalculatorActivity extends AppCompatActivity {
                     }
                 }
                 builder.append(ZERO);
-                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
+                binding.userInputEditText.append(String.valueOf(ZERO));
+                Log.d("cursorAt", String.valueOf(getCursorPosition()));
+                Log.d("length", String.valueOf(binding.userInputEditText.length()));
+
+
             }
         });
 
-        binding.doubleZeroBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!builder.toString().isEmpty()){
-                    if (builder.length() == 1 && TextUtils.equals(String.valueOf(builder.charAt(0)), "0")) {
-                        return; } // end code if true
+//        binding.doubleZeroBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (!builder.toString().isEmpty()){
+//                    if (builder.length() == 1 && TextUtils.equals(String.valueOf(builder.charAt(0)), "0")) {
+//                        return; } // end code if true
+//
+//                    if (checkOperator()){
+//                        builder.append(ZERO);
+//                        canAppendZeroZero = false;
+//                        binding.userInputEditText.setText(builder.toString());
+//
+//                        return;
+//
+//                    }
+//
+//                    if (!canAppendZeroZero){
+//                        return;
+//                    }
+//
+//                    builder.append(ZERO);
+//                }
+//                // continue code
+//                builder.append(ZERO);
+//                binding.userInputEditText.setText(builder.toString());
+//            }
+//        });
 
-                    if (checkOperator()){
-                        builder.append(ZERO);
-                        canAppendZeroZero = false;
-                        binding.userInputEditTextCalculatorActivity.setText(builder.toString());
-
-                        return;
-
-                    }
-
-                    if (!canAppendZeroZero){
-                        return;
-                    }
-
-                    builder.append(ZERO);
-               }
-                // continue code
-                builder.append(ZERO);
-                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
-            }
-        });
 
 
 
         binding.oneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    initZero(ONE);
-                    builder.append(ONE);
-                    canAppendZeroZero = true;
-                    binding.userInputEditTextCalculatorActivity.setText(builder.toString());
-            }
-        });
-
-
-        binding.twoBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                initZero(TWO);
-                builder.append(TWO);
+                initZero(ONE);
+                builder.append(ONE);
+                binding.userInputEditText.append("1");
                 canAppendZeroZero = true;
-                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
-            }
-        });
 
-        binding.threeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                initZero(THREE);
-                builder.append(THREE);
-                canAppendZeroZero = true;
-                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
-            }
-        });
-        binding.fourBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                initZero(FOUR);
-                builder.append(FOUR);
-                canAppendZeroZero = true;
-                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
-            }
-        });
-        binding.fiveBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                initZero(FIVE);
-                builder.append(FIVE);
-                canAppendZeroZero = true;
-                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
-            }
-        });
-        binding.sixBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                initZero(SIX);
-                builder.append(SIX);
-                canAppendZeroZero = true;
-                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
-            }
-        });
-        binding.sevenBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                initZero(SEVEN);
-                builder.append(SEVEN);
-                canAppendZeroZero = true;
-                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
-            }
-        });
-        binding.eightBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                initZero(EIGHT);
-                builder.append(EIGHT);
-                canAppendZeroZero = true;
-                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
-            }
-        });
+                Log.d("cursorAt", String.valueOf(getCursorPosition()));
+                Log.d("length", String.valueOf(binding.userInputEditText.length()));
 
-        binding.nineBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                initZero(NINE);
-                builder.append(NINE);
-                canAppendZeroZero = true;
-                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
-            }
-        });
-
-        binding.decimalPointBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                canAppendZeroZero = true;
-                if (!builder.toString().isEmpty()) {
-                    if (!canDecimal){
-                        return;
-                    }else if(checkOperator()){
-                        builder.append(ZERO);
-                    }
-                }
-                else {
-                    builder.append(ZERO);
-                }
-                builder.append(decimal_point);
-                canDecimal = false;
-                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
 
             }
         });
 
 
-
-        /* To avoid confusion
-         * The operator button logic has been implemented below
-        **/
-
-        binding.addBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (checkForEmptyOrSubtract()){
-                    return;
-                }
-                replaceOperator(ADD);
-                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
-            }
-        });
-
-        binding.subtractBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                replaceOperator(SUBTRACT);
-                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
-            }
-        });
-
-        binding.multiplyBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (checkForEmptyOrSubtract()){
-                    return;
-                }
-                replaceOperator(MULTIPLY);
-                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
-
-            }
-        });
-
-        binding.divideBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (checkForEmptyOrSubtract()){
-                    return;
-                }
-                replaceOperator(DIVIDE);
-                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
-
-            }
-        });
-
-
+//        binding.twoBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                initZero(TWO);
+//                builder.append(TWO);
+//                canAppendZeroZero = true;
+//                binding.userInputEditText.setText(builder.toString());
+//            }
+//        });
+//
+//        binding.threeBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                initZero(THREE);
+//                builder.append(THREE);
+//                canAppendZeroZero = true;
+//                binding.userInputEditText.setText(builder.toString());
+//            }
+//        });
+//        binding.fourBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                initZero(FOUR);
+//                builder.append(FOUR);
+//                canAppendZeroZero = true;
+//                binding.userInputEditText.setText(builder.toString());
+//            }
+//        });
+//        binding.fiveBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                initZero(FIVE);
+//                builder.append(FIVE);
+//                canAppendZeroZero = true;
+//                binding.userInputEditText.setText(builder.toString());
+//            }
+//        });
+//        binding.sixBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                initZero(SIX);
+//                builder.append(SIX);
+//                canAppendZeroZero = true;
+//                binding.userInputEditText.setText(builder.toString());
+//            }
+//        });
+//        binding.sevenBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                initZero(SEVEN);
+//                builder.append(SEVEN);
+//                canAppendZeroZero = true;
+//                binding.userInputEditText.setText(builder.toString());
+//            }
+//        });
+//        binding.eightBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                initZero(EIGHT);
+//                builder.append(EIGHT);
+//                canAppendZeroZero = true;
+//                binding.userInputEditText.setText(builder.toString());
+//            }
+//        });
+//
+//        binding.nineBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                initZero(NINE);
+//                builder.append(NINE);
+//                canAppendZeroZero = true;
+//                binding.userInputEditText.setText(builder.toString());
+//            }
+//        });
+//
+//        binding.decimalPointBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                canAppendZeroZero = true;
+//                if (!builder.toString().isEmpty()) {
+//                    if (!canDecimal){
+//                        return;
+//                    }else if(checkOperator()){
+//                        builder.append(ZERO);
+//                    }
+//                }
+//                else {
+//                    builder.append(ZERO);
+//                }
+//                builder.append(decimal_point);
+//                canDecimal = false;
+//                binding.userInputEditText.setText(builder.toString());
+//
+//            }
+//        });
+//
+//
+//
+//        /* To avoid confusion
+//         * The operator button logic has been implemented below
+//        **/
+//
+//        binding.addBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (checkForEmptyOrSubtract()){
+//                    return;
+//                }
+//                replaceOperator(ADD);
+//                binding.userInputEditText.setText(builder.toString());
+//            }
+//        });
+//
+//        binding.subtractBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                replaceOperator(SUBTRACT);
+//                binding.userInputEditText.setText(builder.toString());
+//            }
+//        });
+//
+//        binding.multiplyBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (checkForEmptyOrSubtract()){
+//                    return;
+//                }
+//                replaceOperator(MULTIPLY);
+//                binding.userInputEditText.setText(builder.toString());
+//
+//            }
+//        });
+//
+//        binding.divideBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (checkForEmptyOrSubtract()){
+//                    return;
+//                }
+//                replaceOperator(DIVIDE);
+//                binding.userInputEditText.setText(builder.toString());
+//
+//            }
+//        });
+//
+//
         binding.cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 builder.setLength(0);
                 canDecimal = true;
-                binding.userInputEditTextCalculatorActivity.setText(builder.toString());
+                binding.userInputEditText.setText(builder.toString());
             }
         });
+//
+//        binding.clearBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                builder.deleteCharAt(getCursorPosition());
+//                binding.userInputEditText.setText(builder.toString());
+//            }
+//        });
 
     }
-
     protected void initZero(int number){
-        if (builder.length() == 1 && TextUtils.equals(String.valueOf(builder.charAt(0)), "0")){
-            builder.setCharAt(0, (char) number);
+        if (!TextUtils.isEmpty(binding.userInputEditText.getText())
+                && binding.userInputEditText.length() == 1
+                && binding.userInputEditText.getText().toString().charAt(0) == '0'){
+
+                    binding.userInputEditText.getText().clear();
+//                    builder.setCharAt(0, (char) number);
         }
     }
 
@@ -294,8 +337,8 @@ public class CalculatorActivity extends AppCompatActivity {
         if (checkOperator()){
             builder.setCharAt(lastIndex, operator.charAt(0)); //// replacing
         } else {
-                builder.append(operator);
-            }
+            builder.append(operator);
+        }
         canDecimal = true;
     }
 
@@ -313,6 +356,12 @@ public class CalculatorActivity extends AppCompatActivity {
     protected boolean checkForEmptyOrSubtract(){
         return builder.toString().isEmpty()
                 || (builder.length() == 1 && builder.charAt(0) == '-');
+    }
+
+
+    protected int getCursorPosition(){
+        return binding.userInputEditText.getSelectionStart();
+
     }
 
 }
